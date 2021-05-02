a80c120e5019bcfeaa409b14f3ad6e4ca5d431dc
Mari-8 <ryanaltimari8@gmail.com>
2021-05-01 22:56:27 -0500

    commit tree a45aa10f50a94d4995feeaa0366e54ea7cadfc8c

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={this.sendCords}>Coords!</button>
+            </div>
+        )
+    }
+
+    sendCords = (event) => {
+        const x = event.clientX 
+        const y = event.clientY
+        this.props.onReceiveCoordinates([x, y])
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={this.delayButton}>Delay</button>
+            </div>
+        )
+    }
+
+    delayButton = (event) => {
+        event.persist()
+        setTimeout(() => {
+            this.props.onDelayedClick(event) 
+
+        }, 
+        this.props.delay
+        )
+            
+    }
+}
